19cca73028522b9fa6c58c6db97acbdb2033cb1a
bvc5434 <bvc5434@psu.edu>
2020-08-28 13:23:25 +0000

    -changed spelling of Celsius and Fahrenheit

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-Ctemp = input("Enter temperature in celcius: ")
+Ctemp = input("Enter temperature in celsius: ")
 Ctemp = (int(Ctemp) + .0)
 Ftemp = ((Ctemp/5)*9)+32
-print(str(Ctemp) + "° in Celcius is equivalent to " + str(Ftemp) + "° Farenheit.")
+print(str(Ctemp) + "° in Celsius is equivalent to " + str(Ftemp) + "° Fahrenheit.")
